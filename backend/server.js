2835129bb594da2e87665cde0bafe1f915785fb6
Rohit Tewari <rtewari056@gmail.com>
2022-11-14 22:22:42 +0530

    Modify folder structure

--- backend/server.js
+++ backend/server.js
@@ -3,38 +3,36 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import cron from "node-cron";
-import cors from "cors";
 
 import connectToMongoDB from "./config/connectToMongoDB.js";
 import { fileRoutes, showRoutes, downloadRoutes } from "./routes/index.js";
 import cronScript from "./config/cronScript.js";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); // The __dirname or __filename global variables are not available in ECMAScript module files.
 const PORT = process.env.PORT || 5002;
 
 const app = express();
 app.use(express.static("public"));
 app.use(express.json());
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, "../.env") });
 connectToMongoDB(); // Connect to Database
 
-// CORS
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(",")
-}
-app.use(cors(corsOptions));
-
-// CRON Job to remove files from upload folder and data from database everyday at midnight 
+// CRON Job to remove files from upload folder and data from database everyday at midnight
 cron.schedule("0 0 * * *", () => {
   cronScript();
 });
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url)); // The __dirname or __filename global variables are not available in ECMAScript module files.
 
 // Template engine
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 // Routes
+app.get("/", (req, res) => {
+  app.use(express.static(path.join(__dirname, "../frontend")));
+  res.sendFile(
+    path.resolve(__dirname, "../", "frontend", "index.html")
+  );
+});
 app.use("/api/files", fileRoutes);
 app.use("/files", showRoutes);
 app.use("/files/download", downloadRoutes);
